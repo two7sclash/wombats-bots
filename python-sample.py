0d6f9b2e924c758021d7a3f44425f85ea931e43f
C.P. Dehli <dehli@users.noreply.github.com>
2017-03-09 09:44:49 -0500

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -1,5 +1,7 @@
 def wombat(state, time_left):
     # Note that the function name MUST be wombat
     return {
-        'action': 'shoot'
+        'command': {
+            'action': 'shoot'
+        }
     }
